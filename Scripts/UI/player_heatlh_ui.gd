eda0ce391cfe0ed997bd4af26d2bc12f4f201573
nick <nick.leslie303@gmail.com>
2025-08-03 00:43:39 -0400

    we won the war against the worst programing I have ever done

--- Scripts/UI/player_heatlh_ui.gd
+++ Scripts/UI/player_heatlh_ui.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var base_heart_ui:Control = get_node("Heart Ui")
 @export var distance:int
 var first_set_health = true
+var health_ui:Array[Control] = []
 
 func _ready() -> void:
 	EventBus.SetPlayerHealth.connect(set_health)
@@ -13,6 +14,9 @@ func set_health(player_health: int):
 	print("Setting Player Health UI to:" + str(player_health))
 	var string = "[font_size={{size}}]Health: {player_health}[/font_size]".format({"player_health":player_health,"size":50})
 	set_counter(string)
+	if health_ui.size() > player_health && health_ui.size() >= 1:
+		var heart =  health_ui.pop_back()
+		heart.queue_free()
 	if first_set_health:
 		for i in range(player_health):
 			print(i)
@@ -28,7 +32,7 @@ func set_health(player_health: int):
 			new_heart.offset_right = -(60 * i)
 			new_heart.offset_left = -(distance * (i + 1))
 			new_heart.offset_top = 100
-
+			health_ui.append(new_heart)
 			add_child(new_heart)
 		base_heart_ui.queue_free() # this sucks but im a bad programer
 		first_set_health = false
